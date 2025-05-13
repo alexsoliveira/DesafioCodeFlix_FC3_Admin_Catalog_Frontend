6f8e123a337c8baf357f11049f26a77f0b9138b5
alexsoliveira <alexsoliveira@gmail.com>
2025-05-13 17:40:16 -0300

    Criando tema e adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import { createTheme } from '@mui/material';
 import { Box, ThemeProvider } from '@mui/system';
 import * as React from 'react';
 import { Hearder } from './components/Hearder';
 import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
-const theme = createTheme({});
+const Home = () => (
+  <Box>
+    <Typography variant='h3' component='h1'>
+      Home
+    </Typography>
+  </Box>
+);
+
+const About = () => (
+  <Box>
+    <Typography variant='h3' component='h1'>
+      About
+    </Typography>
+  </Box>
+);
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={appTheme}>
       <Box
         component='main'
         sx={{
           height: '100vh',
-          // backgroundColor: '#000',
+          backgroundColor: (theme) => theme.palette.grey[900],
         }}
       >
         <Hearder />
         <Layout>
-          <h1>Olá mundo</h1>
+          <h1>Welcome to React Router!</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />            
+          </Routes>
         </Layout>
       </Box>
     </ThemeProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -10,10 +11,12 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <React.StrictMode>    
+    <BrowserRouter>
+      <Provider store={store}>          
+        <App />      
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
