9c44b3dc6363b0f7c2b7f109b724110362a3f940
alexsoliveira <alexsoliveira@gmail.com>
2025-05-15 18:14:03 -0300

    Criando páginas do CRUD de categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import { Box, ThemeProvider } from '@mui/system';
-import * as React from 'react';
 import { Hearder } from './components/Hearder';
 import { Layout } from './components/Layout';
 import { appTheme } from './config/theme';
 import { Routes, Route, Link } from 'react-router-dom';
+import { CategoryList } from './features/categories/ListCategory';
+import { CategoryCreate } from './features/categories/CreateCategory';
+import { CategoryEdit } from './features/categories/EditCategory';
 import { Typography } from '@mui/material';
-
-const Home = () => (
-  <Box>
-    <Typography variant='h3' component='h1'>
-      Home
-    </Typography>
-  </Box>
-);
-
-const About = () => (
-  <Box>
-    <Typography variant='h3' component='h1'>
-      About
-    </Typography>
-  </Box>
-);
 
 function App() {
   return (
@@ -36,8 +22,17 @@ function App() {
         <Layout>
           <h1>Welcome to React Router!</h1>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />            
+            <Route path="/" element={<CategoryList />} />
+            <Route path="/categories" element={<CategoryList />} />
+            <Route path="/categories/create" element={<CategoryCreate />} />
+            <Route path="/categories/edit/:id" element={<CategoryEdit />} />
+
+            <Route path="*" element={
+              <Box sx={{ color: "write" }}>
+                <Typography variant="h1">404</Typography>
+                <Typography variant="h2">Page not found</Typography>                
+              </Box>
+            } />
           </Routes>
         </Layout>
       </Box>
